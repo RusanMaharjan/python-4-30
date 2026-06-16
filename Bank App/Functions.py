e32ecbe2fb3e24a38d145e4312080c9a5d8b5123
Rusan <61351604+RusanMaharjan@users.noreply.github.com>
2026-06-16 16:53:50 +0545

    Python class 4:30 contents

---
+++ Bank App/Functions.py
@@ -0,0 +1,7 @@
+from Storage import all_accounts
+
+def findAccountByNumber(acc_number):
+    for account in all_accounts:
+        if acc_number == account.account_number:
+            return account
+    return None
